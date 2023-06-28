1a44c58e793f7051285d7a5fa3b006665f3604b7
Peter Kure <peterkure256@gmail.com>
2023-06-28 12:28:04 +0300

    Feat: Icons

--- src/Footer.js
+++ src/Footer.js
@@ -6,23 +6,35 @@ function Footer() {
       <div className="row">
         <div className="col-md-4 col-10 mx-auto">
           {/* Social */}
-          <h3 style={styles.heading}>Follow us on social media</h3>
-          <div style={styles.socialIcons}>
-            <span style={styles.icon}>Facebook</span>
-            <span style={styles.icon}>Twitter</span>
-            <span style={styles.icon}>Instagram</span>
+          <div style={styles.section}>
+            <h3 style={styles.heading}>Follow us on social media</h3>
+            <div style={styles.socialIcons}>
+              <a href="#" style={styles.iconLink}>
+                <i className="fab fa-facebook-f"></i>
+              </a>
+              <a href="#" style={styles.iconLink}>
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="#" style={styles.iconLink}>
+                <i className="fab fa-instagram"></i>
+              </a>
+            </div>
           </div>
         </div>
         <div className="col-md-4 col-10 mx-auto">
           {/* Location */}
-          <h3 style={styles.heading}>Our Location</h3>
-          <p style={styles.paragraph}>123 Main Street, City, Country</p>
+          <div style={styles.section}>
+            <h3 style={styles.heading}>Our Location</h3>
+            <p style={styles.paragraph}>123 Main Street, City, Country</p>
+          </div>
         </div>
         <div className="col-md-4 col-10 mx-auto">
           {/* Contact */}
-          <h3 style={styles.heading}>Contact Us</h3>
-          <p style={styles.paragraph}>Email: info@example.com</p>
-          <p style={styles.paragraph}>Phone: 123-456-7890</p>
+          <div style={styles.section}>
+            <h3 style={styles.heading}>Contact Us</h3>
+            <p style={styles.paragraph}>Email: info@example.com</p>
+            <p style={styles.paragraph}>Phone: 123-456-7890</p>
+          </div>
         </div>
       </div>
     </div>
@@ -30,6 +42,11 @@ function Footer() {
 }
 
 const styles = {
+  section: {
+    padding: '20px',
+    borderRadius: '5px',
+    margin: '20px',
+  },
   heading: {
     fontSize: '18px',
     fontWeight: 'bold',
@@ -39,8 +56,10 @@ const styles = {
     display: 'flex',
     marginBottom: '10px',
   },
-  icon: {
+  iconLink: {
     marginRight: '10px',
+    color: '#333',
+    fontSize: '24px',
   },
   paragraph: {
     marginBottom: '5px',
